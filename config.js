a9e9a8eb9f8693b8b63a88979df80955c32a4e83
Francesco D'Abbraccio <fmeater@gmail.com>
2018-09-06 18:38:08 +0200

    More fixes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     'secret': 'ilovescotchyscotch',
-		'database': 'mongodb+srv://<carmine>:<UA0dtEgfGW7Wi84O>@cluster0-v9fhe.mongodb.net/test?retryWrites=true',
-		'database2': 'mongodb://localhost:27017/mytravel'
+		'database': 'mongodb://localhost:27017/mytravel',
+		'database-cloud': 'mongodb+srv://carmine:UA0dtEgfGW7Wi84O@cluster0-v9fhe.mongodb.net/test?retryWrites=true'
 
 };
