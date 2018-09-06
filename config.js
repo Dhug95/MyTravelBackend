39975e7195c41ff5d8261a823e2098f610a8c8db
Francesco D'Abbraccio <fmeater@gmail.com>
2018-09-06 18:20:46 +0200

    Added deployed db

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
 
     'secret': 'ilovescotchyscotch',
-		'database': 'mongodb://localhost:27017/mytravel'
+		'database': 'mongodb+srv://<carmine>:<UA0dtEgfGW7Wi84O>@cluster0-v9fhe.mongodb.net/test?retryWrites=true',
+		'database2': 'mongodb://localhost:27017/mytravel'
 
 };
